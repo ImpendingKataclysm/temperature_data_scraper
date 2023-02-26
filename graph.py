6ede03f8dc432119e179f757fa82f22aa50d6772
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-02-26 13:23:55 -0800

    Optimized code by using pandas to parse csv file

--- graph.py
+++ graph.py
@@ -1,23 +1,13 @@
 import streamlit as st
 import plotly.express as px
+import pandas as pd
 from main import temp_file
 
+df = pd.read_csv(temp_file)
 
-def read(data_file):
-    with open(data_file, "r") as file:
-        return file.readlines()
-
-
-temp_data = read(temp_file)[1:]
-times = []
-temps = []
-
-for item in temp_data:
-    data = item.split(',')
-    times.append(data[0])
-    temps.append(data[1])
-
-temp_graph = px.line(x=times, y=temps, labels={"x": "Date/Time",
-                                               "y": "Average Global Temperature"})
+temp_graph = px.line(x=df["date"],
+                     y=df["temperature"],
+                     labels={"x": "Date/Time",
+                             "y": "Average Global Temperature"})
 st.title("Global Average Temperatures By Date")
 st.plotly_chart(temp_graph)
